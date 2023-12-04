6b44e66c9bd78897dd7cfd96a86627e3d2eafb0e
Sm Kou <66178856+SmKou@users.noreply.github.com>
2023-12-04 00:44:40 -0800

    Add not available message to versions

--- versions/main.js
+++ versions/main.js
@@ -1,5 +1,5 @@
 const versions = [ 'v0/', 'v1/', 'v2/', 'v3/', 'v4/' ]
-const statuses = [ false, false, false, false, true ]
+const statuses = [ true, false, false, false, true ]
 let version = versions.length - 1
 
 const ui = {
@@ -9,7 +9,8 @@ const ui = {
         link: document.getElementById('link'),
         home: document.getElementById('home')
     },
-    frame: document.getElementById('version-view')
+    frame: document.getElementById('version-view'),
+    message: document.querySelector('.message')
 }
 
 ui.frame.setAttribute('src', versions[version])
@@ -18,16 +19,30 @@ ui.controls.prev.addEventListener('click', () => {
     version--
     if (version < 0)
         version = versions.length - 1
-    if (statuses[version])
-        frame.src = versions[version]
+
+    if (statuses[version]) {
+        ui.message.classList.remove('view')
+        ui.frame.src = versions[version]
+    }
+    else {
+        ui.message.classList.add('view')
+        ui.frame.src = ''
+    }
 })
 
 ui.controls.next.addEventListener('click', () => {
     version++
     if (version > versions.length - 1)
         version = 0
-    if (statuses[version])
-        frame.src = versions[version]
+
+    if (statuses[version]) {
+        ui.message.classList.remove('view')
+        ui.frame.src = versions[version]
+    }
+    else {
+        ui.message.classList.add('view')
+        ui.frame.src = ''
+    }
 })
 
 // deployed: https://smkou.github.io/re-portfolio/versions/v4/
