301388abd3dc1c44fd819151e6b622b8b5d34067
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-02-02 14:03:30 -0800

    Add help buttons, bash path, help() and --help to homepage

--- main.js
+++ main.js
@@ -11,11 +11,39 @@ const ui = {
     }
 }
 
+const error = (message, term, type) => `${type}: ${term ? term + ': ' : ''}${message}`
+
+const stdError = (message, term) => error(message, term, 'error')
+const bashError = (message, term) => error(message, term, 'bash')
+const optionError = (message, term) => error(message, term, '')
+
+const addLine = (output, bash = false) => {
+    const line = document.createElement('p')
+    if (bash)
+        line.setAttribute('class', 'bash')
+    line.append(document.createTextNode(output))
+    ui.cns.append(line)
+}
+
+const addLines = output => output.split('\n').forEach(line => addLine(line))
+
 const cmd = {
     cal: function(args) {
-        if (args.length && args.includes('--help')) {
-
+        let date = new Date()
+        if (args.length && args.includes('--help'))
+            return this.help(['cal'])
+        else if (args.length) {
+            try { date = new Date(args[0]) }
+            catch (error) {
+                
+            }
         }
+        addLine(date)
+
+        
+
+        // addLine(error(`unknown option ${args[0]}`, ''))
+        // ls: unknown option -- j
     },
     cd: function(args) {},
     cls: function(args) {},
@@ -24,7 +52,26 @@ const cmd = {
     dir: function(args) {},
     echo: function(args) {},
     find: function(args) {},
-    help: function(args) {},
+    help: function(args) {
+        if (!args.length) {
+            const commands = Object.keys(manual)
+            addLine(`Available commands: ${commands.join(', ')}`)
+            return true
+        }
+
+        if (args[0] === '--help') {
+            addLines(manual.help.help)
+            return true
+        }
+
+        if (!manual.hasOwnProperty(args[0])) {
+            addLine(bashError('command not found', args[0]))
+            return false
+        }
+        
+        addLines(manual[args[0]].help)
+        return true
+    },
     hostname: function(args) {},
     ls: function(args) {},
     lynx: function(args) {},
@@ -38,3 +85,49 @@ const cmd = {
     whatis: function(args) {},
     whoami: function(args) {}
 }
+
+/* ----------------------------------------------------- Setup UI */
+
+const addPath = () => {
+    const path = `Re-Portfolio SMKOU ~/${ui.path.join('/')}`
+    addLine(path, true)
+}
+
+addPath()
+
+ui.aside.tog.addEventListener('click', () => {
+    ui.aside.state = !ui.aside.state
+    const state = ui.aside.state ? 'expanded' : 'collapsed'
+    ui.aside.tog.setAttribute('class', state)
+    ui.aside.e.setAttribute('class', state)
+})
+
+Object.keys(manual).forEach(command => {
+    const btn = document.createElement('button')
+    btn.id = `${command}-btn`
+    btn.append(document.createTextNode(command))
+    ui.aside.ctt.append(btn)
+})
+
+document.querySelectorAll('#aside-content button').forEach(btn => btn.addEventListener('click', () => cmd.help([btn.innerHTML])))
+
+/* ----------------------------------------------------- Enable input */
+
+const execute = input => {
+    const [command, ...ipt] = input.split(' ')
+    if (cmd.hasOwnProperty(command))
+        cmd[command](ipt)
+    else
+        addLine(bashError('command not found', command))
+}
+
+ui.ipt.addEventListener('change', e => {
+    addLine(e.target.value)
+    const value = e.target.value.slice(2)
+    ui.ipt.value = '$ '
+    if (value.includes('&&'))
+        value.split('&&').forEach(val => execute(val.trim()))
+    else
+        execute(value)
+    addPath()
+})
