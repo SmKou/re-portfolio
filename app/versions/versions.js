fd60a35c01474c25a1493db52f4ec5805ca65483
Sm Kou <66178856+SmKou@users.noreply.github.com>
2025-04-16 20:25:27 -0700

    Update versions data

--- app/versions/versions.js
+++ app/versions/versions.js
@@ -2,21 +2,9 @@ const versions = [
 	{
 		title: 'The Profile',
 		href: 'v0/',
-		ref: 'https://www.kamranhughes.com/how-to-tailor-designs-using-the-big-5-personality-traits/',
-		description: 'Re-Portfolio 2015-2017 features a questionnaire that changes page content and layout based on user answers. It is divided into visual preferences, graphic and text preferences, answer sections and question preferences.',
-		dev: `Add user questionnaire
-
-		Content:
-		- chart or bar graph
-		- progress bars
-		- stock image, credit and source
-		- bullet points
-		- personal fact(s)
-		- statement(s)
-		- summary
-		- analysis
-
-		Basis of concept: User Interfaces with Big-5 Personality Traits (refer: ref)`,
+        ref: 'https://agentestudio.com/blog/design-user-profile-page',
+		description: 'Re-Portfolio 2015-2017 features a user profile. The content consists of miscellaneous things like personality test results, appearance, and stats, such as number of projects by status, project progress, and skills.',
+        dev: '',
 		present: true,
 		completed: false
 	},
@@ -24,16 +12,15 @@ const versions = [
 		title: 'The Magazine',
 		href: 'v1/',
 		ref: 'https://www.nytimes.com/section/magazine',
-		description: 'Re-Portfolio 2015-2022 features a magazine-layout of flash fiction, short stories, and series. Though the writing is reflective of mental state at time of writing, do not take them at face value. Assume impressionism.',
-		dev: `Add images for stories (use 640 optimized on clr, gen bw)
-		Edit and redesign smjoker.com (slated)`,
+		description: 'Re-Portfolio 2015-2022 features a magazine-layout of projects. Here, one can find more details on projects, including their status.',
+		dev: '',
 		present: true,
 		completed: false
 	},
 	{
-		title: 'The Design Index',
+		title: 'Design Index',
 		href: 'v2/',
-		description: 'Re-Portfolio 2018-2021 features a book index layout. The index consists of concepts in html and css, which can be further sorted into tags, fonts, shapes, layouts, ui elements, programming, and resources.',
+		description: 'Re-Portfolio 2018-2021 features a book index layout. The index contains notes and useful code snippets, though by useful, snippets like the css may be out of use, so it is mainly for fun and for reference.',
 		dev: `Content:
 		- tags
 		- shapes
@@ -47,22 +34,16 @@ const versions = [
 		completed: false
 	},
 	{
-		title: 'The Show',
+		title: 'Presentation',
 		href: 'v3/',
 		ref: 'https://codepen.io/dodozhang21/pen/kMoXZz',
-		description: 'Re-Portfolio 2020-2022 features four slideshows on portfolio, different page designs, and favorite shows.',
-		dev: `Determine switch mechanism between slideshows
-		Include back and home
-		Add slideshows:
-		- navigation
-		- portfolio
-		- favorite movies
-		- favorite shows`,
+		description: 'Re-Portfolio 2020-2022 features a slideshow, including interests and work ethic.',
+		dev: '',
 		present: false,
 		completed: false
 	},
 	{
-		title: 'The People in Pages',
+		title: 'Pages',
 		href: 'v4/',
 		description: 'Re-Portfolio 2021-2022 features page designs based on observations of people. When I see a combination of clothing that catches my attention, since I cannot draw or take a photograph, I turn their clothing combination into the layout for a webpage.',
 		present: true,
